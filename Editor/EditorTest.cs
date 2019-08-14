440da4302a1be095c63f0131c194d6b5d0a4773b
Àdam Carballo <adam.carballo@outfit7.com>
2019-08-14 16:23:49 +0200

    Random change without tagging

--- Editor/EditorTest.cs
+++ Editor/EditorTest.cs
@@ -2,5 +2,6 @@ namespace AzureUpmTest {
 	public static class EditorTest {
 		public static bool TestProperty { get; set; }
 		public static bool TestProperty2 { get; set; }
+		public static bool TestProperty3 { get; set; }
 	}
 }
